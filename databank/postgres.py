ac149dbfe7791f199be9f0f4196f2501ab07c7d5
Viren Desai <virendesai87@gmail.com>
2022-08-18 13:42:35 -0500

    minor edits

--- databank/postgres.py
+++ databank/postgres.py
@@ -1,20 +1,20 @@
 import psycopg2 as ps
 
-# define credentials
+# postgres credentials
 credentials = {
-    'POSTGRES_ADDRESS': '', # endpoint
-    'POSTGRES_PORT': '', # port
-    'POSTGRES_USERNAME': 'VelesIndiaAdmin',
-    'POSTGRES_PASSWORD': 'VelesIndia0815!',
-    'POSTGRES_DBNAME': 'veles-india-db1'
+    'host': '', # endpoint
+    'port': '', # port
+    'username': 'VelesIndiaAdmin',
+    'password': 'VelesIndia0815!',
+    'database_name': 'veles-india-db1'
 }
 
 # create connection and cursor
-conn = ps.connect(host=credentials['POSTGRES_ADDRESS'],
-                  database=credentials['POSTGRES_DBNAME'],
-                  user=credentials['POSTGRES_USERNAME'],
-                  password=credentials['POSTGRES_PASSWORD'],
-                  port=credentials['POSTGRES_PORT']
+conn = ps.connect(host=credentials['host'],
+                  database=credentials['database_name'],
+                  user=credentials['username'],
+                  password=credentials['password'],
+                  port=credentials['port']
                   )
 
 
